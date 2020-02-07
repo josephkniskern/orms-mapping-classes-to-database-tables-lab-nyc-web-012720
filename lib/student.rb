c8702c3b2185ac12a41bcb1f0673e3a54ac2b361
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-02-07 14:35:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize name, grade, id = nil
+  def initialize name, grade, id
     @name = name
+    @grade = grade
+    @id = nil
   end
 
 
